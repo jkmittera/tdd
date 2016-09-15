571fab57a7071794c26acba59ac839284b04555f
James Kaptuch <james.kaptuch@mittera.com>
2016-09-15 08:52:13 -0500

    First steps of FT for layout and styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -56,7 +56,7 @@ class NewVisitorTest(LiveServerTestCase):
         ## We use a new broser session to make sure that no information of Edith's is 
         ## coming throught from cookies, etc
         import time
-        time.sleep(5)
+        time.sleep(3)
         
         self.browser.quit()
         self.browser = webdriver.Firefox()
@@ -84,3 +84,28 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Use peacock feathers to make a fly', page_text)
 
         # Satisfied, they both go back to sleep
+        time.sleep(3)
+        
+    def test_layout_and_styling(self):
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024,768)
+        
+        # She notices the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 
+            512,
+            delta=10
+        )
+        
+        # She starts a new list and sees the input is nicely centered there too
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2, 
+            512,
+            delta=10
+        )
+        
+        
